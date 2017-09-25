0ebbad768f78094e25984898e2f3c971fb18330a
Jason Miletta <jmiletta01@gmail.com>
2017-09-24 23:07:02 -0500

    Implement saving current world object to JSON file

--- Assets/WorldTileEditor.cs
+++ Assets/WorldTileEditor.cs
@@ -34,7 +34,7 @@ public class WorldTileEditor : MonoBehaviour {
 
             string jsonData = "";
 
-            jsonData = JsonUtility.ToJson(map);
+            jsonData = convertWorldTileGridArrayToJSON(map);
             Debug.Log(jsonData);
             string filePath = Application.dataPath + gameDataProjectFilePath;
             Debug.Log(filePath);
@@ -45,5 +45,56 @@ public class WorldTileEditor : MonoBehaviour {
     public void loadGridFromJSON(string json)
     {
         WorldTileMap newMap = (WorldTileMap)JsonUtility.FromJson(json, System.Type.GetType("WorldTileMap"));
+    }
+
+    private string convertWorldTileGridArrayToJSON(WorldTileMap world)
+    {
+        WorldJSONWrapper jsonWrapper = new WorldJSONWrapper();
+        
+        GameObject[,] gridArray = world.grid;
+
+        int width = (int)world.width;
+        int height = (int)world.height;
+
+        string tileJsonWrapperList = "[";
+        for (var i = 0; i < width; ++i)
+        {
+            for (var j = 0; j < height; ++j)
+            {
+                GridTile tileComponent = gridArray[i, j].GetComponent<GridTile>();
+                TileJSONWrapper tileWrapper = new TileJSONWrapper(tileComponent.currentTileType, i, j);
+                tileJsonWrapperList += JsonUtility.ToJson(tileWrapper) + ", ";
+            }
+        }
+        tileJsonWrapperList = tileJsonWrapperList.TrimEnd(',' , ' ');
+        tileJsonWrapperList += "]";
+        jsonWrapper.width = width;
+        jsonWrapper.height = height;
+        jsonWrapper.tileList = tileJsonWrapperList;
+        
+        return JsonUtility.ToJson(jsonWrapper);
+    }
+
+    private class WorldJSONWrapper
+    {
+        public int width;
+        public int height;
+        
+        public string tileList;
+    }
+
+    private class TileJSONWrapper
+    {
+        public GridTile.tileType tileType;
+        public int xCoor;
+        public int yCoor;
+
+        public TileJSONWrapper(GridTile.tileType type, int x, int y)
+        {
+            tileType = type;
+            xCoor = x;
+            yCoor = y;
+        }
+        
     }
 }
