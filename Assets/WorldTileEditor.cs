9a16cbce6430ce5fbdad54bb2ac5d0278888a6de
Jason Miletta <jmiletta01@gmail.com>
2017-10-21 12:31:17 -0500

    WIP: Add initial work for save/load prompts

--- Assets/WorldTileEditor.cs
+++ Assets/WorldTileEditor.cs
@@ -5,8 +5,14 @@ using System;
 using UnityEngine;
 
 public class WorldTileEditor : MonoBehaviour {
+
+    public enum action { None, Saving, Loading };
+
+    public action currentAction = action.None;
     public Cursor cursor;
     public WorldTileMap worldMap;
+
+    public GameObject saveLoadPrompt;
 
     private string gameDataProjectFilePath = "levelData/levelData.json";
 
@@ -33,11 +39,25 @@ public class WorldTileEditor : MonoBehaviour {
         cursor.cursorDeselect();
     }
 
+    public void promptForSaving()
+    {
+        currentAction = action.Saving;
+        toggleSaveLoadPrompt();
+    }
+
+    public void promptForLoading()
+    {
+        currentAction = action.Loading;
+        toggleSaveLoadPrompt();
+    }
+
     public void saveCurrentGridToJSON()
     {
         //Display text prompt for filename
         string fileName = null;
         WorldJsonUtility.saveMapAsJSON(worldMap, null);
+
+        currentAction = action.None;
     }
 
     //TODO: pull from existing levels in levelData resources and display them in the UI
@@ -62,6 +82,8 @@ public class WorldTileEditor : MonoBehaviour {
 
         //Properly initialize it into the game
         worldMap.updateMapFromJsonWrapper(newMapWrapper);
+
+        currentAction = action.None;
     }
     
     private void destroyCurrentWorld()
@@ -69,6 +91,21 @@ public class WorldTileEditor : MonoBehaviour {
         foreach (GameObject obj in worldMap.grid)
         {
             Destroy(obj);
+        }
+    }
+
+    private void toggleSaveLoadPrompt()
+    {
+        if(saveLoadPrompt != null)
+        {
+            bool isCurrentlyActive = saveLoadPrompt.activeSelf;
+            saveLoadPrompt.SetActive(!isCurrentlyActive);
+
+            //! DO we want to inversely toggle the rest of the UI?
+            //Weird stuff will probably happen if we dont just pause the whole game or handle it
+        } else
+        {
+            Debug.Log("WorldTileEditor - toggleSaveLoadPrompt: No saveLoadPrompt Object was found!");
         }
     }
 }
