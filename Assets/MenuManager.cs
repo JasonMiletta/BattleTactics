f5c5ac48cdfb29d95f177760bad32de2f97254af
Jason Miletta <jmiletta01@gmail.com>
2018-03-27 17:54:34 -0500

    Menu: WIP base for Menu Manager class

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	#region COMPONENTS
+	GameObject Menu;
+	#endregion
+
+	#region EVENTS
+	public delegate void PauseMenuEvent();
+    public static event PauseMenuEvent OnPauseMenuDisplay;
+    public static event PauseMenuEvent OnPauseMenuHide;
+	#endregion
+	// Use this for initialization
+	void Start () {
+		if(Menu == null){
+			Debug.LogError("No Error is attached to the Menu Manager!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			toggleMenu();
+		}
+	}
+
+	
+	public void continueGame(){
+		hideMenu();
+	}
+	public void settings(){
+
+	}
+	public void quitToMenu(){
+
+	}
+	public void quitToDesktop(){
+		Application.Quit();
+	}
+
+	private void toggleMenu(){
+		if(!Menu.activeSelf){
+			showMenu();
+		} else {
+			hideMenu();
+		}
+	}
+	private void showMenu(){
+		Menu.SetActive(true);
+		OnPauseMenuDisplay();
+	}
+
+	private void hideMenu(){
+		Menu.SetActive(false);
+		OnPauseMenuHide();
+	}
+}
