c04a9e59bf3ce1e4d2874a9b4c2df46b6d23c74e
Jason Miletta <jmiletta01@gmail.com>
2018-03-29 17:19:32 -0500

    Main Menu: WIP mock up main menu and connect basic button functionality

---
+++ Assets/Scripts/Game Managers/MainMenuManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+#region COMPONENTS
+	public GameObject Menu;
+	#endregion
+
+	#region EVENTS
+	#endregion
+
+	#region STATE
+	#endregion
+	// Use this for initialization
+	void Start () {
+		if(Menu == null){
+			Debug.LogError("No Error is attached to the Menu Manager!");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void startGame(){
+		SceneManager.LoadScene("TestLevel1");
+	}
+
+	public void startEditor(){
+		SceneManager.LoadScene("TestEditorScene");
+
+	}
+
+	public void openSettings(){
+
+	}
+
+	public void quitToDesktop(){
+		Application.Quit();
+	}
+}
