bac75fced1daa19724e7f8aa4f018f85adefc941
Jason Miletta <jmiletta01@gmail.com>
2018-03-31 11:02:25 -0500

    Structures: WIP spec out base parameters/events for strucutres

--- Assets/Scripts/Gameplay/Structure.cs
+++ Assets/Scripts/Gameplay/Structure.cs
@@ -2,8 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#region 
 public class Structure : MonoBehaviour {
 
+	public enum StructureType {City, Capitol, Barracks, Airstrip, Dock};
+	
+	#region COMPONENTS
+	public GameObject structureModel;
+	#endregion
+
+	#region STATS
+	public StructureType type = StructureType.City;
+	public int totalHealth = 5;
+	#endregion
+
+	#region STATE
+	public int currentHealth = 5;
+	public int teamNumber = 1;
+	public bool isDestroyed = false;
+	public bool isUnowned = true;
+	#endregion
+
+	#region EVENTS
+	public delegate void StructureEvent();
+	public static event StructureEvent OnStructureCreated;
+	public static event StructureEvent OnStructureCaptured;
+	public static event StructureEvent OnStructureDestroyed;
+	#endregion
 	// Use this for initialization
 	void Start () {
 		
